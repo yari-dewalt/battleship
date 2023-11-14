bccac020e81332ee9c560bbb95fcf4a7e5a1e42e
yari-dewalt <yari.dewalt@gmail.com>
2023-11-13 18:25:50 -0800

    Create player class

---
+++ src/player.js
@@ -0,0 +1,49 @@
+import { Gameboard } from "./gameboard.js";
+import { Ship } from "./ship.js";
+
+class Player {
+  constructor() {
+    this.gameboard = new Gameboard();
+    this.ships = [];
+    this.isTurn = true;
+  }
+
+  createShip(length) {
+    ship = new Ship(length);
+    this.ships.push(ship);
+    return ship;
+  }
+
+  makeMove(gameboard, row, col) {
+    // gameboard to receive the attack
+    gameboard.receiveAttack(row, col);
+  }
+}
+
+class Computer {
+  constructor() {
+    this.gameboard = new Gameboard();
+    this.previousAttacks = [];
+    this.isTurn = false;
+  }
+
+  makeMove(gameboard) {
+    let randomRow = Math.floor(Math.random() * 10);
+    let randomCol = Math.floor(Math.random() * 10);
+
+    while (this.hasAttackedBefore(randomRow, randomCol)) {
+      randomRow = Math.floor(Math.random() * 10);
+      randomCol = Math.floor(Math.random() * 10);
+    }
+
+    gameboard.receiveAttack(randomRow, randomCol);
+
+    this.previousAttacks.push({ randomRow, randomCol });
+  }
+
+  hasAttackedBefore(row, col) {
+    return this.previousAttacks.some(
+      (attack) => attack.row === row && attack.col === col
+    );
+  }
+}

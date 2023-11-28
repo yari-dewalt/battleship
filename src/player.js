b9e61f58c692c08d70a1c18132d9e1c7b88fdecc
yari-dewalt <yari.dewalt@gmail.com>
2023-11-27 17:59:48 -0800

    Automatically create comptuer ships and change turn values

--- src/player.js
+++ src/player.js
@@ -16,6 +16,7 @@ class Player {
   makeMove(gameboard, row, col) {
     // gameboard to receive the attack
     gameboard.receiveAttack(row, col);
+    this.isTurn = false;
   }
 }
 
@@ -24,6 +25,46 @@ class Computer {
     this.gameboard = new Gameboard();
     this.previousAttacks = [];
     this.isTurn = false;
+  }
+
+  createShips() {
+    let ships = [];
+    let ship5 = new Ship(5);
+    let ship4 = new Ship(4);
+    let ship3 = new Ship(3);
+    let ship33 = new Ship(3);
+    let ship2 = new Ship(2);
+    ships.push(ship5);
+    ships.push(ship4);
+    ships.push(ship3);
+    ships.push(ship33);
+    ships.push(ship2);
+
+    for (let ship of Object.values(ships)) {
+      let isValidPlacement = false;
+
+      while (!isValidPlacement) {
+        let randomRow = Math.floor(Math.random() * 10);
+        let randomCol = Math.floor(Math.random() * 10);
+        let randomOrientation = Math.floor(Math.random() * 10) % 2;
+        if (randomOrientation === 0) {
+          randomOrientation = "horizontal";
+        } else {
+          randomOrientation = "vertical";
+        }
+        isValidPlacement = this.gameboard.placeShip(
+          ship,
+          randomRow,
+          randomCol,
+          randomOrientation
+        );
+
+        if (isValidPlacement) {
+          break;
+        }
+      }
+    }
+    console.log(this.gameboard.ships);
   }
 
   makeMove(gameboard) {
@@ -38,6 +79,7 @@ class Computer {
     gameboard.receiveAttack(randomRow, randomCol);
 
     this.previousAttacks.push({ randomRow, randomCol });
+    this.isTurn = false;
   }
 
   hasAttackedBefore(row, col) {
